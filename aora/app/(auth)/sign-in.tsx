fd8c6a570ffe8941d08b4ddacfebaf65f07c840c
EucTech <euccreative@gmail.com>
2024-12-21 12:20:58 +0200

    added auth

--- aora/app/(auth)/sign-in.tsx
+++ aora/app/(auth)/sign-in.tsx
@@ -1,14 +1,51 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { images } from "../../constants";
+import FormField from "@/components/FormField";
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center h-full px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[35px] "
+          />
+          <Text className="text-2xl text-white text-semibold  mt-10 font-psemibold">
+            Log in to Aora
+          </Text>
 
-export default SignIn
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e: any) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyBoardType="email-address"
+            placeholder="Enter your email"
+          />
 
-const styles = StyleSheet.create({})
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e: any) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+            placeholder="Enter your password"
+          />
+          
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
+
+const styles = StyleSheet.create({});
