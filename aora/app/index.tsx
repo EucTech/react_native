be8509f7b6fc9c1b74e55ef0f65efd625f28bb95
EucTech <euccreative@gmail.com>
2024-12-21 12:21:24 +0200

    added index page

--- aora/app/index.tsx
+++ aora/app/index.tsx
@@ -1,46 +1,56 @@
-
-import { Image, ScrollView, Text, View } from 'react-native'
-import React from 'react'
-import { Link } from 'expo-router'
-import { StatusBar } from 'expo-status-bar'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { images } from '@/constants'
+import { Image, ScrollView, StatusBar, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "@/constants";
+import CustomButton from "@/components/CustomButton";
+import { Redirect, router } from "expo-router";
 
 const RootLayout = () => {
   return (
-    <SafeAreaView className='bg-primary h-full px-5'>
-      <ScrollView contentContainerStyle={{ height: '100%' }}>
-        <View className='w-full items-center h-full px-4'>
+    <SafeAreaView className="bg-primary h-full px-5">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center items-center h-full px-4">
           <Image
             source={images.logo}
-            className='w-[130px] h-[84px]'
-            resizeMode='contain'
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
           />
 
           <Image
             source={images.cards}
-            className='max-w-[380px] w-full h-[300px]'
-            resizeMode='contain'
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
           />
 
-          <View className='relative mt-5 '>
-            <Text className=' text-3xl text-white font-bold text-center'>Discover Endless Possibilities with{' '}
-              <Text className='text-secondary-200'>Aora</Text>
+          <View className="relative mt-5 ">
+            <Text className=" text-3xl text-white font-bold text-center">
+              Discover Endless Possibilities with{" "}
+              <Text className="text-secondary-200">Aora</Text>
             </Text>
 
-            <Image 
-            source={images.path}
-            className='w-[136px] h-[15px] absolute -bottom-2 -right-8'
-            resizeMode='contain'
-            /> 
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
           </View>
 
-          <Text></Text>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where creativity meets innovation: embark on a journey of limitless
+            exploration with Aora
+          </Text>
+
+          <CustomButton 
+          title="Continue with Email"
+          handlePress={() => router.push("/sign-in")}
+          containerStyles="w-full mt-7"
+          />
         </View>
       </ScrollView>
 
+      <StatusBar backgroundColor="#161622" barStyle="light-content" />
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
