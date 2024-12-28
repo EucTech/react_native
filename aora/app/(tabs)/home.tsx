05685567a9a385a7f04fb78957286e1c6baddfff
EucTech <euccreative@gmail.com>
2024-12-29 01:15:16 +0200

    updated the home

--- aora/app/(tabs)/home.tsx
+++ aora/app/(tabs)/home.tsx
@@ -1,11 +1,29 @@
-import { View, Text } from 'react-native'
+import { FlatList, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView>
+      <FlatList
+      data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={({ item }) => (
+        <Text>{item.id}</Text>
+      )}  
+      ListHeaderComponent={() => (
+        <View className="my-6 px-4 space-y-6" >
+          <View className="justify-between items-center flex-row mb-6">
+            <Text>
+              Welcome back
+            </Text>
+            <Text>JSMastery</Text>
+          </View>
+        </View>
+      )}
+      />
+
+    </SafeAreaView>
   )
 }
 
