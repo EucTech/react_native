ef2cedd4b190925ac1e9895564890b9bb63b9904
EucTech <euccreative@gmail.com>
2024-12-28 22:45:09 +0200

    added global context to always log current user int

--- aora/context/GlobalProvider.tsx
+++ aora/context/GlobalProvider.tsx
@@ -1,25 +1,40 @@
-import { getCurrentUser } from '@/lib/appwrite';
-import { createContext, useContext, useState, useEffect } from 'react';
-
+import { getCurrentUser } from "@/lib/appwrite";
+import { createContext, useContext, useState, useEffect } from "react";
 
 const GlobalContext = createContext({});
 
 export const useGlobalContext = () => useContext(GlobalContext);
 
-const GlobalProvider = ({ children }: any) => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [user, setUser] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
+export const GlobalProvider = ({ children }: any) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        getCurrentUser
-    }, []);
+  useEffect(() => {
+    getCurrentUser()
+        .then((res: any) => {
+            if (res) {
+                setIsLoggedIn(true);
+                setUser(res);
+            } else {
+                setIsLoggedIn(false);
+                setUser(null);
+            }
+        })
+        .catch((error: any) => {
+            console.log(error);
+        })
+        .finally(() => {
+            setIsLoading(false);
+        });
+  }, []);
 
-    return (
-        <GlobalContext.Provider value={{
-
-        }}>
-            {children}
-        </GlobalContext.Provider>
-    )
+  return <GlobalContext.Provider value={{
+    isLoggedIn,
+    setIsLoggedIn,
+    user,
+    setUser,
+    isLoading,
+    setIsLoading
+  }}>{children}</GlobalContext.Provider>;
 };
