b2a067b6135cb97b43dc7a329208795a204702f1
EucTech <euccreative@gmail.com>
2024-12-29 13:36:32 +0200

    updated

--- aora/app/(tabs)/home.tsx
+++ aora/app/(tabs)/home.tsx
@@ -1,30 +1,75 @@
-import { FlatList, Text, View } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { FlatList, Image, RefreshControl, StatusBar, Text, View } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "@/constants";
+import SearchInput from "@/components/SearchInput";
+import Trending from "@/components/Trending";
+import EmptyState from "@/components/EmptyState";
 
 const Home = () => {
+
+  const [refreshing, setRefreshing] = useState(false);
+  
+  const onRefresh = async () => {
+    setRefreshing(true);
+    // re call videos -> if any new videos appeared
+    setRefreshing(false);
+  }
+
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-primary py-4 h-full">
       <FlatList
-      data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
-      keyExtractor={(item) => item.id.toString()}
-      renderItem={({ item }) => (
-        <Text>{item.id}</Text>
-      )}  
-      ListHeaderComponent={() => (
-        <View className="my-6 px-4 space-y-6" >
-          <View className="justify-between items-center flex-row mb-6">
-            <Text>
-              Welcome back
-            </Text>
-            <Text>JSMastery</Text>
+        data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+        // data={[]}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (<Text className="text-3xl text-white">{item.id}</Text>)}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between items-start flex-row mb-6">
+              <View>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  Welcome back
+                </Text>
+                <Text className="text-2xl font-psemibold text-white">
+                  JSMastery
+                </Text>
+              </View>
+              <View className="mt-1.5">
+                <Image 
+                source={images.logoSmall}
+                className="w-9 h-10"
+                resizeMode="contain"
+                />
+              </View>
+            </View>
+
+            <SearchInput
+            placeholder="Search for video topic"
+            />
+
+            <View className="w-full flex-1 pt-5 pb-8">
+              <Text className="text-gray-100 text-lg font-pregularmb-3">
+                Latest Videos
+              </Text>
+
+              <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []} />
+            </View>
           </View>
-        </View>
-      )}
+        )}
+
+        ListEmptyComponent={() => (
+          <EmptyState
+          title="No Videos Found"
+          subtitle="Be the first to upload a video"
+          />
+        )}
+
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
       />
 
+      <StatusBar backgroundColor="#161622" barStyle="light-content" />
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
