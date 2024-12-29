995bd2fed2f02f22e80aab0506b9bee4f8e73da5
EucTech <euccreative@gmail.com>
2024-12-29 15:36:44 +0200

    updated

--- aora/app/(tabs)/home.tsx
+++ aora/app/(tabs)/home.tsx
@@ -1,28 +1,49 @@
-import { FlatList, Image, RefreshControl, StatusBar, Text, View } from "react-native";
-import React, { useState } from "react";
+import {
+  Alert,
+  FlatList,
+  Image,
+  RefreshControl,
+  StatusBar,
+  Text,
+  View,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "@/constants";
 import SearchInput from "@/components/SearchInput";
 import Trending from "@/components/Trending";
 import EmptyState from "@/components/EmptyState";
+import { getAllPosts } from "@/lib/appwrite";
+import useAppwrite from "@/lib/useAppwrite";
+import VideoCard from "@/components/VideoCard";
 
 const Home = () => {
-
   const [refreshing, setRefreshing] = useState(false);
-  
+
+  const { data: posts, refetch } = useAppwrite(getAllPosts);
+  console.log(posts);
+
   const onRefresh = async () => {
     setRefreshing(true);
-    // re call videos -> if any new videos appeared
+    await refetch();
     setRefreshing(false);
-  }
+  };
 
   return (
     <SafeAreaView className="bg-primary py-4 h-full">
       <FlatList
-        data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+        data={posts}
         // data={[]}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (<Text className="text-3xl text-white">{item.id}</Text>)}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <VideoCard
+            video={item.video}
+            title={item.title}
+            thumbnail={item.thumbnail}
+            creator={item.creator.username}
+            avatar={item.creator.avatar}
+          />
+        )}
         ListHeaderComponent={() => (
           <View className="my-6 px-4 space-y-6">
             <View className="justify-between items-start flex-row mb-6">
@@ -35,17 +56,15 @@ const Home = () => {
                 </Text>
               </View>
               <View className="mt-1.5">
-                <Image 
-                source={images.logoSmall}
-                className="w-9 h-10"
-                resizeMode="contain"
+                <Image
+                  source={images.logoSmall}
+                  className="w-9 h-10"
+                  resizeMode="contain"
                 />
               </View>
             </View>
 
-            <SearchInput
-            placeholder="Search for video topic"
-            />
+            <SearchInput placeholder="Search for video topic" />
 
             <View className="w-full flex-1 pt-5 pb-8">
               <Text className="text-gray-100 text-lg font-pregularmb-3">
@@ -56,15 +75,15 @@ const Home = () => {
             </View>
           </View>
         )}
-
         ListEmptyComponent={() => (
           <EmptyState
-          title="No Videos Found"
-          subtitle="Be the first to upload a video"
+            title="No Videos Found"
+            subtitle="Be the first to upload a video"
           />
         )}
-
-        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
       />
 
       <StatusBar backgroundColor="#161622" barStyle="light-content" />
